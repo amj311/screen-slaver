52a59fd952a5eb1adfcec1aef12479cd090bd537
amj311 <amjudd315@gmail.com>
2021-03-24 09:48:07 -0600

    removed unneccessary files

--- app.js
+++ app.js
@@ -7,22 +7,8 @@ var cors = require('cors');
 const cmd = require("node-cmd");
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
-app.use(cors({
-  'allowedHeaders': ['Content-Type', 'API-Key', 'API-Secret', 'Access-Control-Allow-Headers', 'accept', 'client-security-token', 'x-github-event'],
-  'exposedHeaders': ['sessionId'],
-  'origin': '*',
-  'methods': 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
-  'preflightContinue': false,
-  'credentials': true
-}));
-app.options('*', function (req,res) { res.sendStatus(200); });
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -30,9 +16,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(cors());
 
+app.use('/', indexRouter);
 app.post('/git', (req, res) => {
   // If event is "push"
   if (req.headers['x-github-event'] == "push") {
@@ -53,23 +39,6 @@ app.post('/git', (req, res) => {
   }
 
   return res.sendStatus(200); // Send back OK status
-});
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 
