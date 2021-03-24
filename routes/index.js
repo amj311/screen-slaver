e2c9ac7177b44b62438550da96b29d26f246c06d
amj311 <amjudd315@gmail.com>
2021-03-24 12:39:16 -0600

    added write highschools to file

--- routes/index.js
+++ routes/index.js
@@ -7,66 +7,63 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json())
 
-var highscores = [];
+var highscores = require("highscores.json") || [];
 
 
-app.get('/scores', (req,res) => {
+app.get('/scores', (req, res) => {
     let max = parseInt(req.query.max)
-    res.send(highscores.slice(0,max))
+    res.send(highscores.slice(0, max))
 })
 
-app.get('/scores/hi', (req,res) => {
-    res.json(highscores[0])
-})
-
-app.get('/scores/:username', (req,res) => {
-    let userScores = highscores.filter(score => {return score.username === req.params.username})
+app.get('/scores/:username', (req, res) => {
+    let userScores = highscores.filter(score => { return score.username === req.params.username })
     console.log(req.params.username, userScores)
     res.send(userScores)
 })
 
 
-app.post('/scores', (req,res) => {
-    
+app.post('/scores', (req, res) => {
+
     let newScore = parseInt(req.body.score);
     let foundSpot = false;
-    
+
     let idx = 0;
-    
+
     for (; idx < highscores.length; idx++) {
-     	
-     	console.log(idx, highscores[idx].score, `<?`, newScore)
-    	
-    	if (!foundSpot && highscores[idx].score < newScore) {
-     		console.log(`--> ${highscores[idx].score} is less than ${newScore} at ${idx}`)
-     		
-        	req.body.rank = parseInt(idx) + 1;
+
+        if (!foundSpot && highscores[idx].score < newScore) {
+            req.body.rank = parseInt(idx) + 1;
             highscores.splice(idx, 0, req.body)
-    		foundSpot = true;
-    		break;
-    	}
-    	
-     	console.log(idx, highscores[idx].rank, highscores[idx].score, `<?`, newScore)
+            foundSpot = true;
+            break;
+        }
+
     }
-    
-    if (foundSpot){
+
+    if (foundSpot) {
         console.log(idx)
-        
+
         for (; idx < highscores.length; idx++) {
-         	highscores[idx].rank = idx + 1;
+            highscores[idx].rank = idx + 1;
         }
     }
-    
+
     if (!foundSpot) {
         let score = req.body;
         score.rank = highscores.length + 1;
         highscores.push(req.body)
-        // console.log(foundSpot)
     }
-    
-    
- 	console.log(`Inserting. \nHighscores is now: \n`, highscores)
-    
+
+    fs.writeFile("highscores.json", JSON.stringify(highscores), (err) => {
+        if (err)
+            console.log(err);
+        else {
+            console.log("File written successfully\n");
+            console.log("The written has the following contents:");
+            console.log(fs.readFileSync("books.txt", "utf8"));
+        }
+    });
+
     res.send(highscores)
 })
 
